544f13acf4a39a546768827674e9b9ea2474a430
Byshov92 <dimabyshov@gmail.com>
2023-10-16 18:05:52 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginFormEl = document.querySelector(".login-form");
 
+loginFormEl.addEventListener("submit", validateSubmit);
+
+function validateSubmit(event) {
+  event.preventDefault();
+  if (
+    !event.currentTarget.elements.email.value ||
+    !event.currentTarget.elements.password.value
+  ) {
+    return alert("All fields must be filled.!");
+  }
+  else if (
+    !event.currentTarget.elements.email.value.includes("@") ||
+    !event.currentTarget.elements.email.value.includes(".")
+  ) {
+    return alert("Invalide email!");
+  } else {
+    const formData = {
+      email: event.currentTarget.elements.email.value,
+      password: event.currentTarget.elements.password.value,
+    };
+    console.dir(formData);
+    event.currentTarget.reset();
+  }
+}
